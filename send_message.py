b55ea2dc0316c7c6199903cc953bf27bc1bc4817
Sterrrrest <sterrrrest@gmail.com>
2024-05-27 18:21:07 +0300

    Changed output text

--- send_message.py
+++ send_message.py
@@ -6,12 +6,16 @@ def send_message(response, tg_token, tg_chat_id):
     bot = telebot.TeleBot(token=tg_token)
 
     if response['new_attempts'][0]['is_negative']:
-        bot.send_message(chat_id=tg_chat_id, text=f"У вас проверили работу "
-                                               f"'{response['new_attempts'][0]['lesson_title']}'\n\n "
-                                               f"В ней есть ошибки\n\n"
-                                               f"Ссылка на работу: {response['new_attempts'][0]['lesson_url']}")
+        bot.send_message(chat_id=tg_chat_id, text=f'''У вас проверили работу
+                                               '{response['new_attempts'][0]['lesson_title']}'
+                                               
+                                               В ней есть ошибки
+                                               
+                                               Ссылка на работу: {response['new_attempts'][0]['lesson_url']}''')
     else:
-        bot.send_message(chat_id=tg_chat_id, text=f"У вас проверили работу "
-                                               f"'{response['new_attempts'][0]['lesson_title']}'\n\n "
-                                               f"Работа принята!\n\n"
-                                               f"Ссылка на работу: {response['new_attempts'][0]['lesson_url']}")
+        bot.send_message(chat_id=tg_chat_id, text=f'''У вас проверили работу
+                                               '{response['new_attempts'][0]['lesson_title']}'
+                                               
+                                               Работа принята!
+                                               
+                                               Ссылка на работу: {response['new_attempts'][0]['lesson_url']}''')
