c65d63de5a8b73b4258314f08d2942b6ba3879ee
Sterrrrest <sterrrrest@gmail.com>
2024-06-05 15:01:45 +0300

    Formatted code

--- send_message.py
+++ send_message.py
@@ -1,4 +1,5 @@
 import telebot
+from textwrap import dedent
 
 
 def send_message(response, tg_token, tg_chat_id):
@@ -6,16 +7,14 @@ def send_message(response, tg_token, tg_chat_id):
     bot = telebot.TeleBot(token=tg_token)
 
     if response['new_attempts'][0]['is_negative']:
-        text_accept = (f'''У вас проверили работу '{response['new_attempts'][0]['lesson_title']}'
-
-В ней есть ошибки
-
-Ссылка на работу: {response['new_attempts'][0]['lesson_url']}''')
+        text_accept = dedent((f'''
+        У вас проверили работу '{response['new_attempts'][0]['lesson_title']}'
+        В ней есть ошибки
+        Ссылка на работу: {response['new_attempts'][0]['lesson_url']}'''))
 
         bot.send_message(chat_id=tg_chat_id, text=text_accept)
     else:
-        bot.send_message(chat_id=tg_chat_id, text=f'''У вас проверили работу '{response['new_attempts'][0]['lesson_title']}'
-
-Работа принята!
-
-Ссылка на работу: {response['new_attempts'][0]['lesson_url']}''')
+        bot.send_message(chat_id=tg_chat_id, text=dedent(f'''
+        У вас проверили работу '{response['new_attempts'][0]['lesson_title']}'
+        Работа принята!
+        Ссылка на работу: {response['new_attempts'][0]['lesson_url']}'''))
