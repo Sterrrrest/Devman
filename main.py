500261476b4eaadf62450da5854ae7b08108d033
Sterrrrest <sterrrrest@gmail.com>
2024-07-08 16:45:39 +0300

    Sent logs to Telegram to admin user.

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import time
 import requests
+import telebot
+import logging
 
 from send_message import send_message
 from environs import Env
@@ -11,7 +13,24 @@ if __name__ == '__main__':
     env.read_env()
     tg_token = env('TG_TOKEN')
     tg_chat_id = env('TG_CHAT_ID')
+    tg_bot = telebot.TeleBot(token=tg_chat_id)
+
     devman_token = env('DEVMAN_TOKEN')
+
+
+    class TelegramLogsHandler(logging.Handler):
+        def __init__(self, tg_bot, tg_chat_id):
+            super().__init__()
+            self.chat_id = tg_chat_id
+            self.tg_bot = tg_bot
+        def emit(self, record):
+            log_entry = self.format(record)
+            self.tg_bot.send_message(chat_id=self.tg_chat_id, text=log_entry)
+
+
+    logger = logging.getLogger('Logger')
+    logger.setLevel(logging.WARNING)
+    logger.addHandler(TelegramLogsHandler(tg_bot))
 
     url = 'https://dvmn.org/api/user_reviews/'
     url_long = 'https://dvmn.org/api/long_polling/'
@@ -41,3 +60,5 @@ if __name__ == '__main__':
             pass
         except requests.ConnectionError:
             time.sleep(30)
+
+
