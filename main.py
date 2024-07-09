c40fb47265547acd24bedf76d1ee962ffa6b26e0
Sterrrrest <sterrrrest@gmail.com>
2024-07-09 17:33:30 +0300

    Caught errors and write to log

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import time
 import requests
 import telebot
 import logging
+import traceback
 
 from send_message import send_message
 from environs import Env
@@ -13,24 +14,29 @@ if __name__ == '__main__':
     env.read_env()
     tg_token = env('TG_TOKEN')
     tg_chat_id = env('TG_CHAT_ID')
-    tg_bot = telebot.TeleBot(token=tg_chat_id)
+    tg_bot = telebot.TeleBot(token=tg_token)
 
     devman_token = env('DEVMAN_TOKEN')
-
 
     class TelegramLogsHandler(logging.Handler):
         def __init__(self, tg_bot, tg_chat_id):
             super().__init__()
-            self.chat_id = tg_chat_id
+            self.tg_chat_id = tg_chat_id
             self.tg_bot = tg_bot
+
         def emit(self, record):
             log_entry = self.format(record)
             self.tg_bot.send_message(chat_id=self.tg_chat_id, text=log_entry)
 
+    logging.basicConfig(level=logging.DEBUG, format="%(process)d %(levelname)s %(message)s %(asctime)s")
 
-    logger = logging.getLogger('Logger')
-    logger.setLevel(logging.WARNING)
-    logger.addHandler(TelegramLogsHandler(tg_bot))
+    logger = logging.getLogger("Debug")
+
+    logger.addHandler(TelegramLogsHandler(tg_bot, tg_chat_id))
+    logger.setLevel(logging.INFO)
+    logger.info('Старт бота')
+
+
 
     url = 'https://dvmn.org/api/user_reviews/'
     url_long = 'https://dvmn.org/api/long_polling/'
@@ -60,5 +66,7 @@ if __name__ == '__main__':
             pass
         except requests.ConnectionError:
             time.sleep(30)
-
+        except Exception:
+            logger.info('Бот упал с ошибкой:')
+            logger.info(traceback.format_exc())
 
